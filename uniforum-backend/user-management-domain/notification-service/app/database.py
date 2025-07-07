a9a895d8490b417aaa83373b31da4b96748ef61a
AndyAlarconldu <andyjavier3010967@gmail.com>
2025-07-06 20:56:21 -0500

    changes /uniforum-backend

--- uniforum-backend/user-management-domain/notification-service/app/database.py
+++ uniforum-backend/user-management-domain/notification-service/app/database.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://uniforum_user:uniforum123@host.docker.internal:5432/uniforum")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://uniforum_user:uniforum123@172.31.4.157:5432/uniforum")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
