5ff89b6ba08cc72e6d63b04b3a3789d1e2d1effe
AndyAlarconldu <andyjavier3010967@gmail.com>
2025-07-15 22:57:56 -0500

    fixing problems in academic-schedule-service

--- uniforum-backend/course-management-domain/academic-schedule-service/src/app.js
+++ uniforum-backend/course-management-domain/academic-schedule-service/src/app.js
@@ -3,8 +3,12 @@ const cors = require("cors");
 const scheduleRoutes = require("./routes/schedule.routes");
 
 const app = express();
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+  origin: "*", 
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+
+}));app.use(express.json());
 
 app.use("/api/schedules", scheduleRoutes);
 
